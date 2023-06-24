39bf18fb68c3fdf7f966cc575b9191cdce353f8d
Shubham Sharma <85790050+shubham199927@users.noreply.github.com>
2023-06-25 00:26:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+var count = 0;
+
+
+document.addEventListener("keydown", e => {
+
+     var h = e.keyCode;
+     if(h==13){
+        var start = document.getElementById("inputS").value;
+        var end = document.getElementById("inputE").value;
+        var timeStart = document.getElementById('time1').value;
+       
+        var timeEnd = document.getElementById('time2').value;
+   // Conversion of Variables to date objects because js dont treat them as date, but as a string "   "
+   
+     var dateObjectOfStart = new Date(start);
+
+     var dateObjectOfEnd = new Date(end);
+     //timeStart
+     const currentDate = new Date();
+     const [hours,minutes] = timeStart.split(':');
+     currentDate.setHours(hours);
+     currentDate.setMinutes(minutes);
+     currentDate.setSeconds(0);
+    
+ var timeObject1 = currentDate.toTimeString();
+     //timeEnd
+     const currentDate1 = new Date();
+     const [hour, minute] = timeEnd.split(':');
+     currentDate1.setHours(hour);
+     currentDate1.setMinutes(minute);
+     currentDate1.setSeconds(0);
+    
+ var timeObject2 = currentDate1.toTimeString().slice(0, 8);
+
+        console.log(dateObjectOfStart,dateObjectOfEnd);
+      
+        
+        if(start === "" || end === ""){
+         document.getElementById('errors').innerText = "Enter Values first";
+        }
+        else if(start===end){
+         document.getElementById('errors').innerText = "Same date! Please Enter a diffrent date";
+      }
+        else if(start > end){
+         document.getElementById('errors').innerText = "Start date must be lesser than end date! Please Enter a diffrent date";
+        }
+      else{
+         document.getElementById('errors').innerText = "";
+      
+      
+    function updateTime(){
+     
+      // currentDate.setMinutes(currentDate.getMinutes() + 1);
+      currentDate.setSeconds(currentDate.getSeconds() + 1);
+      timeObject1 = currentDate.toTimeString().slice(0, 8);
+
+      if(timeObject1 === "23:59:59"){
+         count++;
+         if(count>0){
+            dateObjectOfStart.setDate( dateObjectOfStart.getDate()+1 );
+
+         }
+      }
+ 
+
+      
+      document.getElementById('day').innerText = dateObjectOfStart ;
+      document.getElementById('clock').innerText = timeObject1 ;
+      console.log(count);
+     
+      if (dateObjectOfStart.getTime() === dateObjectOfEnd.getTime() && timeObject1 === timeObject2){
+         clearInterval(intervalId);
+         console.log("worked");
+
+      }
+    
+      
+      
+    }
+    const intervalId = setInterval(updateTime,10);
+
+     
+     
+     
+
+
+    
+   }
+
+     }
+
+});
+
+document.addEventListener("click",e =>{
+   document.getElementById('errors').innerText = "";
+})
